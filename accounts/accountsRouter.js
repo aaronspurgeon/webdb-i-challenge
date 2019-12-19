b4dce1e74e6fbcb18b6b3665b0b269fe2891b8c3
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-12-18 20:05:45 -0500

    Added endpoint to delete an account

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -56,4 +56,15 @@ router.put("/:id", async (req, res, next) => {
   }
 });
 
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await db("accounts")
+      .where("id", req.params.id)
+      .del();
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
