d80e4b1868b2980d5a5e2c8964c5d1a3de5c0818
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-12-18 19:32:46 -0500

    Added dependencies and three endpoints, totally forgot to commit, hectic day

---
+++ accounts/accountsRouter.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const db = require("../data/dbConfig");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    res.json(await db("accounts").select());
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    res.json(
+      await db("accounts")
+        .where("id", req.params.id)
+        .first()
+    );
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget
+    };
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const server = require('./server.js');
+const server = require("./server.js");
+const accountsRouter = require("./accounts/accountsRouter");
 
 const PORT = process.env.PORT || 4000;
+
+server.use("/api/accounts", accountsRouter);
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Something went wrong."
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
